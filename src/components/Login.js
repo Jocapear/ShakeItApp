ddc243bd9e81a00a8906dff537f92b670e94c043
Hermes <hermes.espinola@gmail.com>
2019-09-10 18:39:39 -0500

    Google signin setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from '../logo.svg';
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 const Login = () => {
-    return (
+    return [
+        <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />,
         <div>
             <img src={logo} className="App-logo" alt="logo" width="125" height="125" />
             <label for="email">Email</label>
@@ -10,7 +22,7 @@ const Login = () => {
             <label for="password">Password</label>
             <input type="password" name="password" />
         </div>
-    );
+    ];
 };
 
 export default Login;
