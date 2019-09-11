2f3f5df7cf21532ab47872a080806daa411abf18
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2019-09-10 20:04:23 -0500

    Improved map component and added icons and style

---
+++ src/components/StoreMap.js
@@ -0,0 +1,44 @@
+import React, { useCallback, useRef } from 'react'
+import L, { geoJSON } from 'leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import '../styles/StoreMap.css'
+
+import icon from '../images/store_icon_color.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [38, 38],
+  shadowSize: [38, 38],
+  iconAnchor: [19, 38],
+  shadowAnchor: [5, 38],
+  popupAnchor: [0, -38]
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
+
+const StoreMap = ({ attribution, url }) => {
+  const map = useRef(null)
+
+  return (
+    <Map
+      boundOptions={{ padding: [100, 100] }}
+      center={[20.7326234, -103.4297673]}
+      zoom={15}
+      ref={map}
+    >
+      <TileLayer url={url} attribution={attribution} />
+      <Marker position={[20.7326234, -103.4297673]}></Marker>
+    </Map>
+  )
+}
+
+StoreMap.defaultProps = {
+  url:
+    'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+}
+
+export default StoreMap
