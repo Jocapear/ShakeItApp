57c7c44281b404b4799ffd4bd1662fb717e39e66
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2019-10-08 20:04:06 -0500

    Linter commands fixed for windows

--- src/components/CouponFetcher.js
+++ src/components/CouponFetcher.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { string, number, object } from 'prop-types';
 import Coupon from './Coupon';
 
 const CouponFetcher = () => {
   return (
     <div>
-      <div class="massive ui olive animated fade button" tabindex="0">
-        <div class="visible content">Shake it!</div>
-        <div class="hidden content"> Get a promo</div>
+      <div className="massive ui olive animated fade button" tabIndex="0">
+        <div className="visible content">Shake it!</div>
+        <div className="hidden content"> Get a promo</div>
       </div>
       <Coupon></Coupon>
-      
     </div>
   );
 };
 
-CouponFetcher.defaultProps = {
-  
-};
+CouponFetcher.defaultProps = {};
 
 export default CouponFetcher;
